381ee401b2041a4aabbd720375087aac2280ed32
MartynasPupkus <pupkus.martynas@gmail.com>
2024-01-22 09:16:05 +0200

    Darbas su gui

---
+++ PTU20_live/gui_labas_martynas.py
@@ -0,0 +1,25 @@
+import PySimpleGUI as sg
+
+layout = [
+    [sg.Text('Koks tavo vardas?')],
+    [sg.Input(key = "-NAME-")],
+    [
+        sg.Button('Passisveikinti', key = "-HELLO-"),
+        sg.Button('Atsisveikinti', key = "-BYE-"),
+    ],
+    [sg.Text(size=(40, 1), key ='-OUTPUT-')]
+]
+
+window = sg.Window('LABAS', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WINDOW_CLOSED:
+        break
+
+    if event == '-HELLO-':
+        window['-OUTPUT-'].update(f'Sveiki {values["-NAME-"]}')
+    elif event == '-BYE-':
+        window['-OUTPUT-'].update(f'Viso gero {values["-NAME-"]}')
+
+    window.close
